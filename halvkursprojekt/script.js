b9ff16ab51dea5b2bc9edba2dea653014da892f7
Berzan13 <berhin0814@edu.halmstad.se>
2025-12-02 09:13:31 +0100

    lektion 2 dec

--- halvkursprojekt/script.js
+++ halvkursprojekt/script.js
@@ -6,22 +6,28 @@ const sidebar = document.querySelector('.sidebar')
 const products = [
     {
         label: 'Geneva Dark Blue',
-        img: 'https://thegallerist.se/cdn/shop/files/Darkbluesida.jpg?v=1754412245&width=535',
+        img: 'https://thegallerist.se/cdn/shop/files/milanodarkbluemain_4dda6bb4-4ea3-42ba-800b-66ee87122172.jpg?v=1761041574&width=535',
         price: 2499
     },
     {
         label: 'Geneva White',
-        img: 'https://thegallerist.se/cdn/shop/files/Darkbluesida.jpg?v=1754412245&width=535',
+        img: 'https://thegallerist.se/cdn/shop/files/WHITE_45x2_1cafa5c1-2242-4a4d-80df-36e4a071b7fd.jpg?v=1759245708&width=360',
         price: 2399
     },
      {
-        label: 'Moritz Summer',
-        img: 'https://thegallerist.se/cdn/shop/files/Darkbluesida.jpg?v=1754412245&width=535',
+        label: 'Vienna Dark Blue',
+        img: 'https://thegallerist.se/cdn/shop/files/viennadarkbluemain.jpg?v=1761045861&width=535',
         price: 1999
     },
      {
-        label: 'Moritz Winter',
-        img: 'https://thegallerist.se/cdn/shop/files/Darkbluesida.jpg?v=1754412245&width=535',
+        label: 'Madrid Summer',
+        img: 'https://thegallerist.se/cdn/shop/files/MadridSneaker.jpg?v=1747932695&width=535',
+        price: 2699
+    },
+
+     {
+        label: 'Vienna Black',
+        img: 'https://thegallerist.se/cdn/shop/files/svartnymain.jpg?v=1760952076&width=535',
         price: 2699
     },
 
@@ -32,10 +38,29 @@ const products = [
     },
 
      {
-        label: 'Moritz Winter',
-        img: 'https://thegallerist.se/cdn/shop/files/Darkbluesida.jpg?v=1754412245&width=535',
+        label: 'Moritz Brown',
+        img: 'https://thegallerist.se/cdn/shop/files/Brown_outside.jpg?v=1760082343&width=535',
         price: 2699
     },
+
+     {
+        label: 'Moritz Brown',
+        img: 'https://thegallerist.se/cdn/shop/files/gronnymain.jpg?v=1760952379&width=535',
+        price: 2699
+    },
+
+    {
+        label: 'Moritz Brown',
+        img: 'https://thegallerist.se/cdn/shop/files/nyblaskomainbild.jpg?v=1755701694&width=535',
+        price: 2699
+    },
+
+    {
+        label: 'Moritz Brown',
+        img: 'https://thegallerist.se/cdn/shop/files/milanonavymain.jpg?v=1761035801&width=535',
+        price: 2699
+    },
+
 ]
 
 
@@ -44,14 +69,14 @@ let productsHTML = '';
 products.forEach(product => {
     productsHTML += `
     <div class="product">
-                    <div class="imgBox">
-                            <img src="${product.img}" alt="">
-                        </div>
-                        <div class="textBox">
-                            <h4>${product.label}</h4>
-                            <h6>${product.price.toLocaleString('sv-SE')} sek</h6>
-                        </div>
-                    </div>`
+        <div class="imgBox">
+                    <img src="${product.img}" alt="">
+               </div>
+        <div class="textBox">
+                <h4>${product.label}</h4>
+                <h6>${product.price.toLocaleString('sv-SE')} sek</h6>
+              </div>
+    </div>`
 })
 
 grid.innerHTML = productsHTML
@@ -60,3 +85,4 @@ grid.innerHTML = productsHTML
 function toggleMenu() {
     sidebar.classList.toggle('active')
 }
+
